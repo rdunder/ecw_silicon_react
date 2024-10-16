1cb5fe87d0d83e814c06d11694d5733e9d87df4d
rdunder <r.dunder@gmail.com>
2024-10-16 16:23:12 +0200

    working on features

--- src/sections/features/features.jsx
+++ src/sections/features/features.jsx
@@ -2,45 +2,58 @@
 
 import './features.css';
 
-function SectionFeatures() {
+import logo1 from './logo1.svg';
+import logo2 from './logo2.svg';
+import logo3 from './logo3.svg';
+import logo4 from './logo4.svg';
+import logo5 from './logo5.svg';
+import logo6 from './logo6.svg';
 
+function SectionFeatures() {
+    const brandLogos = [logo1, logo2, logo3, logo4, logo5, logo6];
 
     return (
-    <section class="section-features">
-        <div class="logo-container container">
-            <div class="logo">
+    <section className="section-features">
+        <div className="logo-container container">
+            {brandLogos.map(logo => (
+                <div key={logo} className="logo">
+                    <img src={logo} alt="logotype" />
+                </div>
+            ))}
+
+            {/* <div className="logo">
                 <img src="./assets/p2 logo 1.svg" alt="logotype" />
             </div>
-            <div class="logo">
+            <div className="logo">
                 <img src="./assets/p2 logo 2.svg" alt="logotype" />
             </div>
-            <div class="logo">
+            <div className="logo">
                 <img src="./assets/p2 logo 3.svg" alt="logotype" />
             </div>
-            <div class="logo">
+            <div className="logo">
                 <img src="./assets/p2 logo 4.svg" alt="logotype" />
             </div>
-            <div class="logo">
+            <div className="logo">
                 <img src="./assets/p2 logo 5.svg" alt="logotype" />
             </div>
-            <div class="logo">
+            <div className="logo">
                 <img src="./assets/p2 logo 6.svg" alt="logotype" />
-            </div>
+            </div> */}
         </div>
-        <div id="Features" class="feature-large-container container">
-            <div class="img-iphone">
+        <div id="Features" className="feature-large-container container">
+            <div className="img-iphone">
                 <img src="./assets/p2 img iPhone 12 Pro.png" alt="graphical image of an iphone" />
             </div>
 
             <div>
-                <div class="features-content">
+                <div className="features-content">
                     <h2>App Features</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin volutpat mollis egestas. Nam luctus facilisis ultrices. Pellentesque volutpat ligula est. Mattis fermentum, at nec lacus.</p>
                 </div>
 
-                <div class="feature-container">
-                    <div class="feature">
-                        <div class="ft-img">
+                <div className="feature-container">
+                    <div className="feature">
+                        <div className="ft-img">
                             <img src="./assets/p2 logo easy payments.svg" aria-hidden="true" />
                         </div>
                         <div>
@@ -48,8 +61,8 @@ function SectionFeatures() {
                             <p>Id mollis consectetur congue egestas egestas suspendisse blandit justo.</p>
                         </div>
                     </div>
-                    <div class="feature">
-                        <div class="ft-img">
+                    <div className="feature">
+                        <div className="ft-img">
                             <img src="./assets/p2 logo data security.svg" aria-hidden="true" />
                         </div>
                         <div>
@@ -57,8 +70,8 @@ function SectionFeatures() {
                             <p>Id mollis consectetur congue egestas egestas suspendisse blandit justo.</p>
                         </div>
                     </div>
-                    <div class="feature">
-                        <div class="ft-img">
+                    <div className="feature">
+                        <div className="ft-img">
                             <img src="./assets/p2 logo cost statistics.svg" aria-hidden="true" />
                         </div>
                         <div>
@@ -66,8 +79,8 @@ function SectionFeatures() {
                             <p>Id mollis consectetur congue egestas egestas suspendisse blandit justo.</p>
                         </div>
                     </div>
-                    <div class="feature">
-                        <div class="ft-img">
+                    <div className="feature">
+                        <div className="ft-img">
                             <img src="./assets/p2 logo support 247.svg" aria-hidden="true" />
                         </div>
                         <div>
@@ -75,8 +88,8 @@ function SectionFeatures() {
                             <p>Id mollis consectetur congue egestas egestas suspendisse blandit justo.</p>
                         </div>
                     </div>
-                    <div class="feature">
-                        <div class="ft-img">
+                    <div className="feature">
+                        <div className="ft-img">
                             <img src="./assets/p2 logo regular cashback.svg" aria-hidden="true" />
                         </div>
                         <div>
@@ -84,8 +97,8 @@ function SectionFeatures() {
                             <p>Sit facilisis dolor arcu, fermentum vestibulum arcu elementum imperdiet eleifend.</p>
                         </div>
                     </div>
-                    <div class="feature">
-                        <div class="ft-img">
+                    <div className="feature">
+                        <div className="ft-img">
                             <img src="./assets/p2 logo top standards.svg" aria-hidden="true" />
                         </div>
                         <div>
